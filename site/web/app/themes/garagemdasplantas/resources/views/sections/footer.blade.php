38a68a4298401c91769821ba651d89c125c5fb8b
Daniel Semblano <dsemblano@gmail.com>
2026-07-10 19:12:09 -0300

    Footer improvements

--- site/web/app/themes/garagemdasplantas/resources/views/sections/footer.blade.php
+++ site/web/app/themes/garagemdasplantas/resources/views/sections/footer.blade.php
@@ -1,10 +1,10 @@
-<footer id="footer" class="content-info bg-verde text-black py-6">
+<footer id="footer" class="content-info bg-secondary text-white py-6">
     <div
-        class="container max-w-none text-black prose:a:text-black">
+        class="container max-w-none text-white prose:a:text-white">
         @php(dynamic_sidebar('sidebar-footer'))
         <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2 text-black">
             <span class="z-10 font-bold text-white">© 2025 - {{ date('Y') }} 
-                <a class="text-black hover:underline text-white" href="{{ home_url('/') }}">Garagem das Plantas</a>
+                <a class="hover:underline text-white" href="{{ home_url('/') }}">Garagem das Plantas</a>
                 <span id="trademark" class="sup align-text-bottom">&reg;</span>
             </span>
         </div>
